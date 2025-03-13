86c31cf3643ab928384c0f9b0473fc79bbaad274
SheryllNome <77122248+zamivn@users.noreply.github.com>
2025-03-13 11:51:04 -0400

    cataloug3

---
+++ midterm/js/catalouge.js
@@ -0,0 +1,22 @@
+const catalougRef = document.querySelector("#catalouge")
+const catalouge = [
+    {image:
+        "images/i210-midterm-product1.webp",
+    name: "firstfire",
+    },
+    {image: "images/i210-midterm-product6.webp",
+        name: "handy"
+
+    },{image:
+        "images/i210-midterm-product2.webp.webp",
+    name: "nap",}
+    ,{image:
+        "images/iw210-midterm-product3.webp",
+    name: "Tub",}
+    ,{image:
+        "images/i210-midterm-product4.webp",
+    name: "toilet",}
+    ,{image:
+        "images/i210-midterm-product5.webp",
+    name: "pouch",}
+]
